448847526833dd0f2c2647e467e7a4419de25367
Marvin <mhc9yq@virginia.edu>
2021-04-04 14:12:46 -0400

    travis

--- project/settings.py
+++ project/settings.py
@@ -98,11 +98,11 @@ DATABASES = {
 }
 
 if 'test' in sys.argv:
-DATABASES['default'] = {
-    'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-         'TEST_NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
- }
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+          'TEST_NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 
 
 # Password validation
