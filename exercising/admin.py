8ccab801d825cf20f7195911077aeaf2dfc1ecb8
Marvin <mhc9yq@virginia.edu>
2021-04-05 14:48:03 -0400

    added initial test cases

--- exercising/admin.py
+++ exercising/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Profile, Exercise_Log
 from django.contrib.auth.admin import UserAdmin
 
+
 # Register your models here.
 
 admin.site.register(Profile)
